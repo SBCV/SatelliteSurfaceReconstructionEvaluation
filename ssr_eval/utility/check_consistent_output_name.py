534c9ce34796a64466f0054b4f6975894c080d4c
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2025-02-28 19:03:39 +0100

    Improve consistent output name check

--- ssr_eval/utility/check_consistent_output_name.py
+++ ssr_eval/utility/check_consistent_output_name.py
@@ -1,15 +1,30 @@
 import os
+import re
+
+
+def _extract_number(s, pattern):
+    match = re.match(pattern, s)
+    if match:
+        number = match.group(1)
+        return number
+    else:
+        assert False, f"{s} vs {pattern}"
 
 
 def _verify_odp_matches_data_idp(odp_site_prefix, site_data_idp):
     # site_data_idp: path/to/siteX
     # odp_site_prefix: MVS3D_site_X
-    assert_msg = f"odp_site_prefix: {odp_site_prefix} vs. site_data_idp: {site_data_idp}"
     # Ensure the index of the dataset matches the index of the odp
-    site_odp_index = odp_site_prefix[-1]
-    site_data_index = site_data_idp[-1]
-    assert site_odp_index in ["1", "2", "3"]
-    assert site_data_index in ["1", "2", "3"]
+    site_odp_index = _extract_number(
+        odp_site_prefix, pattern=r"^site_(\d+)"
+    )
+    site_data_idn = os.path.basename(site_data_idp)
+    site_data_index = _extract_number(
+        site_data_idn, pattern=r"^site(\d+)"
+    )
+    assert site_odp_index in ["1", "2", "3"], f"Did not find a valid {site_odp_index} in {odp_site_prefix}"
+    assert site_data_index in ["1", "2", "3"], f"Did not find a valid {site_data_index} in {site_data_idn}"
+    assert_msg = f"odp_site_prefix: {odp_site_prefix} vs. site_data_idp: {site_data_idp}"
     assert site_odp_index == site_data_index, assert_msg
 
 
